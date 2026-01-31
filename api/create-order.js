959e09f7481e220d5c3e55fa45b91a8a6257b289
trioncee <trioncee@gmail.com>
2026-01-31 17:43:27 +0530

    Update create-order origin

--- api/create-order.js
+++ api/create-order.js
@@ -17,7 +17,8 @@ module.exports = async (req, res) => {
     }
 
     // CORS
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    //res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
